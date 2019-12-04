6f69d88a369a33397eea32edb541b84a18c7caed
Tim Hopper <tdhopper@gmail.com>
2019-12-03 22:59:30 -0500

    try fixing error

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ all: deploy
 .PHONY: deploy
 deploy:
 	STATIC_DEPS=true pip3 install -U twitter requests nodb -t .
+	pip3 uninstall futures
+	pip3 uninstall concurrent
 	serverless deploy
 
 .PHONY: clean
--- tweet.py
+++ tweet.py
@@ -8,12 +8,12 @@ from random import shuffle
 
 tweet_content = "tweet-content.txt"
 tweet_lookup = "tweet-lookup.shelve"
-logging.basicConfig(filename='tweet.log', level=logging.WARNING)
+logging.basicConfig(filename="tweet.log", level=logging.WARNING)
 
 
 def get_raw_tweets():
     tweets = []
-    with open(tweet_content, 'r') as f:
+    with open(tweet_content, "r") as f:
         raw_tweets = f.readlines()
         for tweet in raw_tweets:
             tweets.append(tweet.strip())
@@ -22,10 +22,10 @@ def get_raw_tweets():
 
 def send_tweet(s):
     cred = {
-        "consumer_key": os.environ['CONSUMER_KEY'].strip(),
-        "consumer_secret": os.environ['CONSUMER_SECRET'].strip(),
-        "token": os.environ['TOKEN'].strip(),
-        "token_secret": os.environ['TOKEN_SECRET'].strip(),
+        "consumer_key": os.environ["CONSUMER_KEY"].strip(),
+        "consumer_secret": os.environ["CONSUMER_SECRET"].strip(),
+        "token": os.environ["TOKEN"].strip(),
+        "token_secret": os.environ["TOKEN_SECRET"].strip(),
     }
     auth = tw.OAuth(**cred)
     t = tw.Twitter(auth=auth)
@@ -33,14 +33,10 @@ def send_tweet(s):
     print("Sent tweet: {}".format(s))
 
     db = get_db()
-    db.save({"content": s,
-             "last_sent": int(time.time()),
-             "from": "machen"})
+    db.save({"content": s, "last_sent": int(time.time()), "from": "machen"})
 
 
-def get_db(bucket="olneyhymnbots",
-           serializer="json",
-           index="content"):
+def get_db(bucket="olneyhymnbots", serializer="json", index="content"):
     db = NoDB()
     db.bucket = bucket
     db.human_readable_indexes
@@ -57,9 +53,8 @@ def tweets_ordered_by_last_sent_time():
         if d is None:
             last_sent[tweet] = 0
         else:
-            last_sent[tweet] = d['last_sent']
-    s = sorted([(time, tweet)
-                for tweet, time in last_sent.items()])
+            last_sent[tweet] = d["last_sent"]
+    s = sorted([(time, tweet) for tweet, time in last_sent.items()])
     return [t for _, t in s]
 
 
@@ -72,5 +67,5 @@ def tweet(a, b):
     send_tweet(tweet)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tweet()
